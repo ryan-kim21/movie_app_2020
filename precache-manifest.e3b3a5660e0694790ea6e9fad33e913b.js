0cd38e1db90f3a6194fb290d3382ab3e8fcb6649
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2020-08-23 22:19:33 +0900

    Updates

---
+++ precache-manifest.e3b3a5660e0694790ea6e9fad33e913b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992c1fd9c101912aa2fc31c910e57bf3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0f718b37a5efed5ffb62",
+    "url": "/movie_app_2020/static/css/main.6705fa33.chunk.css"
+  },
+  {
+    "revision": "36cd93b10d7d5fb61658",
+    "url": "/movie_app_2020/static/js/2.9ef3c0e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.9ef3c0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f718b37a5efed5ffb62",
+    "url": "/movie_app_2020/static/js/main.dbe3aaae.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
